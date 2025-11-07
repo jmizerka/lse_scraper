4f9002a34eaf0f453c91f49a7d6f8646308541d1
jmizerka <j.mizerka98@gmail.com>
2025-11-07 20:25:00 +0100

    simplify stock processor

--- app/core/stock_processor.py
+++ app/core/stock_processor.py
@@ -1,13 +1,6 @@
-import asyncio
-
 class StocksProcessor:
-    def __init__(self, crawler, semaphore_limit: int = 5):
-        self.semaphore = asyncio.Semaphore(semaphore_limit)
+    def __init__(self, crawler):
         self.crawler = crawler
 
-    async def process_stocks(self, stocks):
-        return await asyncio.gather(*[self.get_stock_data(stock) for stock in stocks])
-
-    async def get_stock_data(self, stock: dict):
-        async with self.semaphore:
-            return self.crawler.get_stock_data(stock)
+    async def process_stocks(self, stocks: list[dict]) -> list[dict]:
+        return await self.crawler.crawl_all(stocks)
